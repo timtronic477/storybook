ea3b010ffd05a81ef4e6a3571e5b70a479a01fd8
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2024-09-22 20:13:08 -0700

    added layouts

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require("dotenv")
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db')
 
 
@@ -9,6 +11,17 @@ dotenv.config({ path:"./config/config.env"})
 connectDB()
 
 const app = express()
+ 
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs.engine());
+app.set('view engine', '.hbs')
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 8000
 
