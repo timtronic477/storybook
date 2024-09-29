8a71bbe61dcf3f92e0790bd770fdccb602c12145
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2024-09-28 23:34:40 -0700

    added edit function

--- app.js
+++ app.js
@@ -59,6 +59,12 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set global var
+app.use(function(req, res, next){
+  res.locals.user = req.user || null
+  next()
+})
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
